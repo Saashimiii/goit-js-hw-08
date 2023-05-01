d6b8dbb821b3ec513509f45379fa86edc422bedd
Saashimiii <abaginskaya@icloud.com>
2023-05-01 16:34:10 +0300

    feedback 8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,45 +1,24 @@
-function onInput(evt) {
-  const target = evt.target;
-  target.nodeName === 'INPUT'
-    ? (email = target.value)
-    : (message = target.value);
-  localStorage.setItem(
-    'feedback-form-state',
-    JSON.stringify({
-      email,
-      message,
-    })
-  );
-  valueObj.email = email;
-  valueObj.message = message;
+import throttle from 'lodash.throttle';
+import debounce from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const input = form.email;
+const textArea = form.message;
+const userData = {
+  email: '',
+  message: '',
+};
+
+// Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message, у яких зберігай поточні значення полів форми. Нехай ключем для сховища буде рядок "feedback-form-state".
+form.addEventListener('input', throttle(onInput, 500, { trailing: false }));
+function onInput(event) {
+  const userData = {
+    email: input.value.trim(),
+    message: event.currentTarget.elements.message.value.trim(),
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(userData));
 }
-
-// додано перевірку наявності даних в localStorage
-if (localStorage.getItem('feedback-form-state')) {
-  const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
-  if (savedData.email) {
-    email = savedData.email;
-    input.value = email;
-  }
-  if (savedData.message) {
-    message = savedData.message;
-    textarea.value = message;
-  }
-}
-
-if (localStorage.getItem('feedback-form-state')) {
-  const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
-  if (savedData.email) {
-    email = savedData.email;
-    input.value = email;
-  }
-  if (savedData.message) {
-    message = savedData.message;
-    textarea.value = message;
-  }
-}
-
-
 // input.addEventListener('focus', onFocus);
 // textArea.addEventListener('focus', onFocus);
 
@@ -56,3 +35,34 @@ if (localStorage.getItem('feedback-form-state')) {
 //     }
 //   }
 // }
+
+// Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
+if (JSON.parse(localStorage.getItem('feedback-form-state'))) {
+  const localData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  input.value = Object.values(localData)[0];
+  textArea.value = Object.values(localData)[1];
+}
+
+// Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  if (email.value.trim() === '' || message.value.trim() === '') {
+    return alert("Шановний(а), будь ласка, обов'язково заповніть всі поля!");
+  } else {
+    localStorage.removeItem('feedback-form-state');
+    console.log({
+      email: email.value.trim(),
+      message: message.value.trim(),
+    });
+    input.value = '';
+    textArea.value = '';
+  }
+}
